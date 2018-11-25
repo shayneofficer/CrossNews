aeec85a314e9ce0b023fe7315352a065873ed260
shayneofficer <shayneofficer@gmail.com>
2018-11-24 20:41:48 -0600

    fixed crossword JSON query

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,18 +1,16 @@
 // ==================================================================================================================
 // Retrieve crossword info from the GitHub archive
-var year = 2000;
-var month = 6;
-var day = 15;
+var year = "2000";
+var month = "06";
+var day = "15";
 
 var crossWordURL = `https://raw.githubusercontent.com/doshea/nyt_crosswords/master/${year}/${month}/${day}.json`;
 
-$.ajax({
-    url: crossWordURL,
-    method: "GET"
-}).then(function (response) {
-    // Console log the response object for testing purposes
-    console.log(response);
+$.getJSON(crossWordURL, function (data) {
+    // Console log the data object for testing purposes
+    // console.log(data);
 });
+
 // ==================================================================================================================
 // Retrieve weather info from the Dark Sky API
 
@@ -30,7 +28,7 @@ var weatherSecond = "00";
 
 
 var weatherKey = "ec5b98b7b3c4b26cd294595db6f0a868"
-var weatherURL = `https://api.darksky.net/forecast/${weatherKey}/${weatherLattitude},${weatherLongitude},${weatherYear}-${weatherMonth}-${weatherDay}T${weatherHour}:${weatherMinute}:${weatherSecond}`;
+var weatherURL = `https://api.darksky.net/forecast/${weatherKey}/${weatherLattitude},${weatherLongitude},${weatherYear}-${weatherMonth}-${weatherDay}T12:00:00`;
 
 $.ajax({
     url: weatherURL,
