efe28a57dc2eedf64970b1afcba0f06d372443d2
Ryan BrodeFrank <ryanafrank@gmail.com>
2018-11-26 21:20:51 -0600

    added js to display weather stats for historic day in weather.html

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -66,12 +66,12 @@ function newDate(date) {
     $("#date-historic").text(moment(date).format("MM/DD/YYYY"));
     year = "" + moment(date).year();
     month = "" + (moment(date).month() + 1);
-    if(month.length<2) {
+    if (month.length < 2) {
         month = "0" + month
     }
-    
+
     day = "" + moment(date).date();
-    if(day.length<2) {
+    if (day.length < 2) {
         day = "0" + day
     }
     console.log(`${month}/${day}/${year}`);
@@ -79,136 +79,162 @@ function newDate(date) {
 
 console.log(`m/d/y ${month}/${day}/${year}`);
 
-var crossWordURL = `https://raw.githubusercontent.com/doshea/nyt_crosswords/master/${year}/${month}/${day}.json`;
+// var crossWordURL = `https://raw.githubusercontent.com/doshea/nyt_crosswords/master/${year}/${month}/${day}.json`;
 
-$.ajax({
-    url: crossWordURL,
-    method: "GET"
-}).then(function (response) {
-    // ===============================================================================================================
-    // Crossword Display
-    response = JSON.parse(response);
-    console.log("CrossWord Creation:")
-    var rows = response.size.rows;
-    var cols = response.size.cols;
+// $.ajax({
+//     url: crossWordURL,
+//     method: "GET"
+// }).then(function (response) {
+//     // ===============================================================================================================
+//     // Crossword Display
+//     response = JSON.parse(response);
+//     console.log("CrossWord Creation:")
+//     var rows = response.size.rows;
+//     var cols = response.size.cols;
 
-    console.log(response);
-    console.log(`rows: ${rows}`);
-    console.log(`cols: ${cols}`);
+//     console.log(response);
+//     console.log(`rows: ${rows}`);
+//     console.log(`cols: ${cols}`);
 
-    //Figure out board dimensions
-    var crosswordHolder = $("<div class='grid-holder'>");
-    //Calculate square sizes
-    //Square Creation
-    for (var i = 0; i < rows; i++) {
-        var newRow = $("<div class='row-holder'>");
-        for (var j = 0; j < cols; j++) {
-            var count = i * cols + j;
-            //Assign Letter Value/Clue Number Value
-            var letterHolder = $("<div class='letter-holder'>");
-            letterHolder.attr("data-letter", response.grid[count]);
-            letterHolder.attr("data-clue-number", response.gridnums[count]);
+//     //Figure out board dimensions
+//     var crosswordHolder = $("<div class='grid-holder'>");
+//     //Calculate square sizes
+//     //Square Creation
+//     for (var i = 0; i < rows; i++) {
+//         var newRow = $("<div class='row-holder'>");
+//         for (var j = 0; j < cols; j++) {
+//             var count = i * cols + j;
+//             //Assign Letter Value/Clue Number Value
+//             var letterHolder = $("<div class='letter-holder'>");
+//             letterHolder.attr("data-letter", response.grid[count]);
+//             letterHolder.attr("data-clue-number", response.gridnums[count]);
 
-            //Formating Cell
-            if (response.grid[count] === "." || count >= response.grid.length) {
-                letterHolder.css("background-color", "black");
-            } else if (response.gridnums[count] <= 0) {
-                letterHolder.html(`<div class='grid-letter'>${response.grid[count]}</div>`/*+ "<br>" + count*/);
-            } else {
-                letterHolder.html(`<div class='grid-number'>${response.gridnums[count]}</div><div class='grid-letter'>${response.grid[count]}</div>` /*+ "<br>" + count*/);
-            }
+//             //Formating Cell
+//             if (response.grid[count] === "." || count >= response.grid.length) {
+//                 letterHolder.css("background-color", "black");
+//             } else if (response.gridnums[count] <= 0) {
+//                 letterHolder.html(`<div class='grid-letter'>${response.grid[count]}</div>`/*+ "<br>" + count*/);
+//             } else {
+//                 letterHolder.html(`<div class='grid-number'>${response.gridnums[count]}</div><div class='grid-letter'>${response.grid[count]}</div>` /*+ "<br>" + count*/);
+//             }
 
-            newRow.append(letterHolder);
-        }
-        crosswordHolder.append(newRow);
-    }
-    $("#crossword").append(crosswordHolder);
+//             newRow.append(letterHolder);
+//         }
+//         crosswordHolder.append(newRow);
+//     }
+//     $("#crossword").append(crosswordHolder);
 
-    //Crosswords Hints   
-    var acrossClues = $("<div class='col s6' id='across-clues'>");
-    acrossClues.html("<strong>Across</strong>");
+//     //Crosswords Hints   
+//     var acrossClues = $("<div class='col s6' id='across-clues'>");
+//     acrossClues.html("<strong>Across</strong>");
 
-    var downClues = $("<div class='col s6' id='down-clues'>");
-    downClues.html("<strong>Down</strong>");
+//     var downClues = $("<div class='col s6' id='down-clues'>");
+//     downClues.html("<strong>Down</strong>");
 
-    for (var i = 0; i < response.clues.across.length; i++) {
-        var newClue = $("<div class='clue'>");
-        newClue.attr("data-answer", response.answers.across[i]);
-        newClue.text(response.clues.across[i]);
-        acrossClues.append(newClue);
-    }
+//     for (var i = 0; i < response.clues.across.length; i++) {
+//         var newClue = $("<div class='clue'>");
+//         newClue.attr("data-answer", response.answers.across[i]);
+//         newClue.text(response.clues.across[i]);
+//         acrossClues.append(newClue);
+//     }
 
-    for (var i = 0; i < response.clues.down.length; i++) {
-        var newClue = $("<div class='clue'>");
-        newClue.attr("data-answer", response.answers.down[i]);
-        newClue.text(response.clues.down[i]);
-        downClues.append(newClue);
-    }
+//     for (var i = 0; i < response.clues.down.length; i++) {
+//         var newClue = $("<div class='clue'>");
+//         newClue.attr("data-answer", response.answers.down[i]);
+//         newClue.text(response.clues.down[i]);
+//         downClues.append(newClue);
+//     }
 
-    $("#hints").append(acrossClues);
-    $("#hints").append(downClues);
-});
+//     $("#hints").append(acrossClues);
+//     $("#hints").append(downClues);
+// });
 
 
 // ==================================================================================================================
 // Retrieve weather info from the Dark Sky API
 
-// (Chicago) lattitude & longitude
-var weatherLattitude = "41.881832";
-var weatherLongitude = "-87.623177";
+function weatherCall() {// (Chicago) lattitude & longitude
+    var weatherLattitude = "41.881832";
+    var weatherLongitude = "-87.623177";
 
-// Day of weather (set to same date as crossword)
-var weatherYear = year;
-var weatherMonth = month;
-var weatherDay = day;
+    // Day of weather (set to same date as crossword)
+    var weatherYear = year;
+    var weatherMonth = month;
+    var weatherDay = day;
+    console.log(`Weather date: ${weatherMonth}/${weatherDay}/${weatherYear}`);
 
+    var weatherKey = "ec5b98b7b3c4b26cd294595db6f0a868"
+    var weatherURL = `https://api.darksky.net/forecast/${weatherKey}/${weatherLattitude},${weatherLongitude},${weatherYear}-${weatherMonth}-${weatherDay}T12:00:00?exclude=currently,minutely,hourly,flags`;
 
-var weatherKey = "ec5b98b7b3c4b26cd294595db6f0a868"
-var weatherURL = `https://api.darksky.net/forecast/${weatherKey}/${weatherLattitude},${weatherLongitude},${weatherYear}-${weatherMonth}-${weatherDay}T12:00:00?exclude=currently,minutely,hourly,flags`;
+    $.ajax({
+        url: weatherURL,
+        method: "GET",
+        dataType: "jsonp"
+    }).then(function (response) {
+        // Console log the response object for testing purposes
+        console.log(response);
 
-$.ajax({
-    url: weatherURL,
-    method: "GET",
-    dataType: "jsonp"
-}).then(function (response) {
-    // Console log the response object for testing purposes
-    console.log(response);
+        var weather = response.daily.data[0];
+        console.log(weather);
+
+        $("#weather-icon").html(`<img src="assets/images/weather-icons/${weather.icon}.jpg" alt="${weather.icon} icon">`);
+        $("#weather-summary").text(`${weather.summary}`);
+        $("#wind").text(`${weather.windSpeed} MPH Wind Speed`);
+        var humidity = weather.humidity * 100
+        $("#humidity").text(`${humidity}% Humidity`);
+        var temp = Math.round((weather.temperatureHigh + weather.temperatureLow)/2);
+        $("#temp").html(`${temp}&#8457;`);
+        var cloudCover = weather.cloudCover * 100;
+        $("#cloud-cover").text(`${cloudCover}% Cloud Cover`);
+        if(typeof weather.precipType != "undefined") {
+            var precip = weather.precipProbability * 100;
+            $("#precip").text(`${precip}% chance of ${weather.precipType}`);
+        } else {
+            $("#precip").empty();
+        }
+
+    });
+}
+
+$("#weather-btn").on("click", function(){
+    weatherCall();
 });
 
+weatherCall();
 // ==================================================================================================================
 // Retrieve article info from the New York Times Article Search API
 
 // Day of headline (set to same date as crossword & weather)
-var headlineYear = year;
-var headlineMonth = month;
-var headlineDay = day;
+// var headlineYear = year;
+// var headlineMonth = month;
+// var headlineDay = day;
 
-var nytURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-nytURL += '?' + $.param({
-    'api-key': "b9f91d369ff59547cd47b931d8cbc56b:0:74623931",
-    'fl': "headline",
-    'begin_date': (headlineYear + headlineMonth + headlineDay),
-    'end_date': (headlineYear + headlineMonth + headlineDay)
-});
+// var nytURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+// nytURL += '?' + $.param({
+//     'api-key': "b9f91d369ff59547cd47b931d8cbc56b:0:74623931",
+//     'fl': "headline",
+//     'begin_date': (headlineYear + headlineMonth + headlineDay),
+//     'end_date': (headlineYear + headlineMonth + headlineDay)
+// });
 
-$.ajax({
-    url: nytURL,
-    method: "GET",
-}).then(function (response) {
-    // Console log response for testing purposes
-    console.log(response);
-}).fail(function (err) {
-    throw err;
-});
+// $.ajax({
+//     url: nytURL,
+//     method: "GET",
+// }).then(function (response) {
+//     // Console log response for testing purposes
+//     console.log(response);
+// }).fail(function (err) {
+//     throw err;
+// });
 
 // ==================================================================================================================
 
-var horoscopeURL = "https://www.horoscopes-and-astrology.com/json";
+// var horoscopeURL = "https://www.horoscopes-and-astrology.com/json";
 
-$.ajax({
-    url: horoscopeURL,
-    method: "GET"
-}).then(function (response) {
-    // Console log response for testing purposes
-    console.log(response);
-});
+// $.ajax({
+//     url: horoscopeURL,
+//     method: "GET"
+// }).then(function (response) {
+//     // Console log response for testing purposes
+//     console.log(response);
+// });
